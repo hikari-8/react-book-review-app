71900de75bab2bc52f61318961c2abf1a059bec2
hikari-8 <kanbarusuruga0127@gmail.com>
2023-06-15 20:55:34 +0900

    add validation for input area of signin/up

--- src/components/Signin.tsx
+++ src/components/Signin.tsx
@@ -36,8 +36,8 @@ const SignIn: React.FC = () => {
     <>
       <form onSubmit={handleSubmitNewUser} className='flex flex-col space-y-4 mt-48'>
         <div className='text-center font-bold text-lg'>ログイン</div>
-        <input type="email" name="email" placeholder='Email' value={user.email} onChange={handleChange} className='mx-auto w-96 p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500'  />
-        <input type="password" name="password" placeholder="Password"  value={user.password} onChange={handleChange} className='mx-auto w-96 p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500' />
+        <input type="email" name="email" placeholder='Email' value={user.email} onChange={handleChange} required className='mx-auto w-96 p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500'  />
+        <input type="password" name="password" placeholder="Password"  value={user.password} onChange={handleChange} required className='mx-auto w-96 p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500' />
         <div className='mx-auto'>
           <button type="submit" className='block w-32 justify-center items-center text-white font-bold bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 mt-10'>Sign In</button>
         </div>
--- src/components/Signup.tsx
+++ src/components/Signup.tsx
@@ -33,9 +33,9 @@ const SignUp: React.FC = () => {
   return (
     <form onSubmit={handleSubmit} className='flex flex-col space-y-4 mt-48'>
       <div className='text-center font-bold text-lg'>新規登録</div>
-      <input type="text" name="name" value={user.name} onChange={handleChange} className='mx-auto w-96 p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500' placeholder='Please enter your name' />
-      <input type="email" name="email" value={user.email} onChange={handleChange} className='mx-auto w-96 p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500' placeholder='Please enter your email'/>
-      <input type="password" name="password" value={user.password} onChange={handleChange} className='mx-auto w-96 p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500' placeholder='Please enter password' />
+      <input type="text" name="name" value={user.name} onChange={handleChange} required className='mx-auto w-96 p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500' placeholder='Please enter your name' />
+      <input type="email" name="email" value={user.email} onChange={handleChange} required className='mx-auto w-96 p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500' placeholder='Please enter your email'/>
+      <input type="password" name="password" value={user.password} onChange={handleChange} required className='mx-auto w-96 p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500' placeholder='Please enter password' />
       <div className='mx-auto'>
         <button type="submit" className='block w-32 justify-center items-center text-white font-bold bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 mt-10'>Sign Up</button>
       </div>
