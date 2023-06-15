56116e39b64699600af7b25b7032dd332fd19f27
hikari-8 <kanbarusuruga0127@gmail.com>
2023-06-15 09:37:56 +0900

    finish e2e test at signin/up

--- src/components/Signin.cy.tsx
+++ src/components/Signin.cy.tsx
@@ -1,10 +1,15 @@
 import React from "react";
-import { mount } from "@cypress/react";
+import { mount } from "@cypress/react18";
 import Signin from "./Signin";
+import { BrowserRouter as Router } from "react-router-dom";
 
 describe("Signin", () => {
   beforeEach(() => {
-    mount(<Signin />);
+    mount(
+      <Router>
+        <Signin />
+      </Router>
+    );
   });
 
   it("displays error message with invalid inputs", () => {
@@ -17,12 +22,12 @@ describe("Signin", () => {
     cy.get('input[name="email"]').type("john@example.com");
     cy.get('input[name="password"]').type("mypassword");
     cy.get('button[type="submit"]').click();
-    // routing
+  });
+
+  it("valid routing to next page", ()=> {
     cy.intercept({
       method: "GET",
-      url: "users/*", // that have a URL that matches '/users/*'
-    },
-  []// and force the response to be: []
-  ).as("getUsers") // and assign an alias
-  });
+      url: "users/*"
+    })
+  }) 
 });
--- src/components/Signup.cy.tsx
+++ src/components/Signup.cy.tsx
@@ -1,10 +1,21 @@
 import React from "react";
-import { mount } from "@cypress/react";
+import { mount } from "@cypress/react18";
 import Signup from "./Signup";
+import { BrowserRouter as Router } from "react-router-dom";
 
 describe("Signup", () => {
   beforeEach(() => {
-    mount(<Signup />);
+    mount(
+      <Router >
+        <Signup />
+      </Router>
+    );
+  });
+
+  it("displays error message with invalid inputs", () => {
+    cy.get('input[name="email"]').type("not an email");
+    cy.get('input[name="password"]').type("short");
+    cy.get('button[type="submit"]').click();
   });
 
   it("does not display error messages with valid inputs", () => {
@@ -13,5 +24,12 @@ describe("Signup", () => {
     cy.get('input[name="password"]').type("mypassword");
     cy.get('button[type="submit"]').click();
   });
+
+  it("valid routing to next page", ()=> {
+    cy.intercept({
+      method: "GET",
+      url: "users/*"
+    })
+  }) 
 });
 export {};
